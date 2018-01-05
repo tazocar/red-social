bb6276cb042fde58020013e6d3ce5f10013fd58e
tazocar <tankodani@gmail.com>
2018-01-05 12:10:47 -0300

    fixing local links

--- assets/js/app.js
+++ assets/js/app.js
@@ -150,7 +150,7 @@ btnSignUp.addEventListener("click", e => {
 btnLogIn.addEventListener("click", function(){
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-      self.location="https://tazocar.github.io/red-social/user-index.html";
+      self.location="user-index.html";
     } else {
       console.log(false);
     }
@@ -159,9 +159,4 @@ btnLogIn.addEventListener("click", function(){
 
 btnLogOut.addEventListener("click", e => {
   firebase.auth().signOut();
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (!firebaseUser) {
-      self.location="https://tazocar.github.io/red-social/index.html";
-    }
-  });
 });
